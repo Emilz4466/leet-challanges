e0ade75f03f746295b69d94d12f5a71125bbabb3
Emilz4466 <58122836+Emilz4466@users.noreply.github.com>
2023-09-15 15:01:03 +0200

    two_sum_fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        // 1. Two sum
+        TwoSum twoSum = new TwoSum();
+        int[] nums = new int[]{3,3};
+        int target = 6;
+        int[] result = twoSum.twoSum(nums, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
     }
 }
