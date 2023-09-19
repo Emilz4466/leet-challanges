63ad6f4bcf746b57dbcc48a1426b531b826a1b69
Emil Zyberski <e.zyberski@streamsoft.pl>
2023-09-19 15:34:23 +0200

    basic_calculator_II_01_init+first_try

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,16 @@ public class Main {
         int[] result = twoSum.twoSum(nums, target);
         System.out.println("1. Two sum \n [" + result[0] + ", " + result[1] + "]\n\n\n");
 
-        // 2. AddTwoNumbers 16.09.2023
+        // 2. AddTwoNumbers 16.09.2023 - 17.09.2023
         AddTwoNumbers atn = new AddTwoNumbers();
         AddTwoNumbers.ListNode l1 = new AddTwoNumbers.ListNode(2, new AddTwoNumbers.ListNode(4, new AddTwoNumbers.ListNode(3)));
         AddTwoNumbers.ListNode l2 = new AddTwoNumbers.ListNode(5, new AddTwoNumbers.ListNode(6, new AddTwoNumbers.ListNode(4)));
 
         AddTwoNumbers.ListNode res = atn.addTwoNumbers(l1, l2);
+
+        // 227. Basic Calculator II 19
+        BasicCalculatorII bcII = new BasicCalculatorII();
+        System.out.println("Basic calc 2");
+        System.out.println(bcII.print(bcII.basicCalc("3+2*2")));
     }
 }
