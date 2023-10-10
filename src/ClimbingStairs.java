9a8b2737397bb9a4860c020ae5f870932191c593
Emilz4466 <emil.z4466@gmail.com>
2023-10-10 22:24:58 +0200

    climbing_stairs_02_fix

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -27,8 +27,7 @@ public class ClimbingStairs {
 
      */
 
-    static int fib(int n)
-    {
+    static int fib(int n) {
         if (n <= 1)
             return n;
         return fib(n - 1) + fib(n - 2);
